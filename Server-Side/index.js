78f636bf75c3ea0d001cc417ee268c2fd3984d84
Alvaro <AlvaroDSolari19@outlook.com>
2022-09-30 08:11:42 -0500

    Established MongoDB Connection

---
+++ Server-Side/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const cors = require('cors'); 
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+const connectToMongo = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI); 
+        console.log('Connected to MongoDB')
+    } catch (anError) { 
+        console.error(anError); 
+        process.exit(1); 
+    }
+
+}
+
+
+
+connectToMongo(); 
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Listening on port ${process.env.PORT || 8080}`);
+});
